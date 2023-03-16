01ecf4463b713f8cae3dc21c6f59a26aebb0fa4b
Paul Bonilla <Paul.Bonilla@verndale.com>
2023-03-16 18:46:14 -0500

    Update screenshots reports

--- utils/screenshots_reports.py
+++ utils/screenshots_reports.py
@@ -1,14 +1,18 @@
 import allure
 import os
+import pathlib
 from datetime import datetime
+
 
 class ScreenshotsReports:
 
     def __init__(self, driver):
         self.driver = driver
-    
+
     def add_screenshot(self, test_name, screenshot_name):
-        file_name = f'reports/{test_name}{datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}.png'
+        current_directory = pathlib.Path(__file__).parent.parent.resolve()
+        file_name = f'{current_directory}\\reports\\{test_name}{datetime.now().strftime("%d-%m-%Y_%H-%M-%S")}.png'
         self.driver.get_screenshot_as_file(file_name)
-        allure.attach.file(file_name, name=screenshot_name, attachment_type=allure.attachment_type.PNG)
+        allure.attach.file(file_name, name=screenshot_name,
+                           attachment_type=allure.attachment_type.PNG)
         os.remove(file_name)
